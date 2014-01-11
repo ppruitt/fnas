2b7dbbc6eb830982ad939817ac6d98441962dd75
Paul Pruitt <paul@paulpruitt.org>
2014-01-11 16:46:00 -0500

    Added tmux

--- scripts/install_pkg.sh
+++ scripts/install_pkg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pkg install bash emacs-nox11 git samba4 netatalk3 nginx avahi smartmontools openldap-server rsync
+pkg install bash emacs-nox11 git samba4 netatalk3 nginx avahi smartmontools openldap-server rsync tmux
