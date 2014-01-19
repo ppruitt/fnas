07663341f4956f1768e3707cadc240e56cc1716f
Paul Pruitt <paul@paulpruitt.org>
2014-01-19 12:45:17 -0500

    - Roll recursive total sizes into total_size field for directories. - Visit directories in alpha-order

--- scripts/make_tree_fingerprints.py
+++ scripts/make_tree_fingerprints.py
@@ -8,38 +8,29 @@ import stat
 import cPickle
 import logging
 
-def compute_dir_hashes(dir_entry, cursor, hash_dict):
-    '''Computes hashes for directories'''
-    if len(dir_entry[4]) > 0:
-        # sort child vector by directory name
-        dir_entry[4].sort(key = lambda x: x[2])
+# Directory entry indexes
+(D_IDX_ID, 
+ D_IDX_PARENT_ID, 
+ D_IDX_PATH, 
+ D_IDX_STAT, 
+ D_IDX_CHILD_DIRS, 
+ D_IDX_CHILD_FILES, 
+ D_IDX_HASH, 
+ D_IDX_TOTAL_SIZE) = range(8)
 
-        # compute hashes for all children
-        for child_dir in dir_entry[4]:
-            logging.debug('Recurse %s' % child_dir[2])
-            compute_dir_hashes(child_dir, cursor, hash_dict)
+# File entry indexes
+(F_IDX_PATH,
+ F_IDX_ID,
+ F_IDX_DIR_ID,
+ F_IDX_STAT,
+ F_IDX_HASH) = range(5)
 
-    # hash all child file and directory hashes
-    h = hashlib.sha256()
+BYTES_PER_LOG_MESSAGE = 1024 * 1024 * 1024
+FILES_PER_COMMIT = 1000
+HASH_READ_BLOCKSIZE = 1024 * 1024
 
-    logging.debug('Hashing %s..' % dir_entry[2])
+last_log_bytes = 0
 
-    for file_ent in dir_entry[5]:
-        assert file_ent[4] is not None
-        h.update(file_ent[4])
-        
-    for child in dir_entry[4]:
-        assert child[6] is not None
-        h.update(child[6])
-
-    hash_str = h.hexdigest()
-    dir_entry[6] = hash_str
-
-    logging.debug('Hash for %s is %s' % (dir_entry[2], dir_entry[6]))
-    hash_dict.setdefault(hash_str, []).append(dir_entry[2]) 
-    cursor.execute('update files set hash = ? where id = ?',
-                   (hash_str, dir_entry[0]))
-    
 def pretty_bytes(bytes):
     '''Print bytes in friendly units'''
     (MB, GB) = (1024**2, 1024**3)
@@ -48,15 +39,48 @@ def pretty_bytes(bytes):
     else:
         return '%0.2f MB' % (float(bytes) / MB)
 
-BYTES_PER_LOG_MESSAGE = 1024 * 1024 * 1024
-last_log_bytes = 0
-
 def log_progress(cur_path, file_count, byte_count) :
     global last_log_bytes
     if (byte_count - last_log_bytes > BYTES_PER_LOG_MESSAGE):
         last_log_bytes = byte_count
         logging.info('%s bytes read, %d files, processing: %s' % (pretty_bytes(byte_count), file_count, cur_path))
  
+def compute_dir_hashes(dir_entry, cursor, hash_dict):
+    '''Computes hashes for directories'''
+    if len(dir_entry[D_IDX_CHILD_DIRS]) > 0:
+        # compute hashes for all children
+        for child_dir in dir_entry[D_IDX_CHILD_DIRS]:
+            logging.debug('Recurse %s' % child_dir[D_IDX_PATH])
+            compute_dir_hashes(child_dir, cursor, hash_dict)
+
+    # hash all child file and directory hashes
+    h = hashlib.sha256()
+
+    # Size of a directory is the recursive sum of all child files and child directories
+    total_size = 0
+
+    logging.debug('Hashing %s..' % dir_entry[D_IDX_PATH])
+    
+    # hash the hashes of all child files in alpha order of their names
+    for file_ent in dir_entry[D_IDX_CHILD_FILES]:
+        assert file_ent[F_IDX_HASH] is not None
+        h.update(file_ent[F_IDX_HASH])
+        total_size += file_ent[F_IDX_STAT].st_size
+
+    # hash the hashes of all child dirs in alpha order of their names
+    for child in dir_entry[D_IDX_CHILD_DIRS]:
+        assert child[D_IDX_HASH] is not None
+        h.update(child[D_IDX_HASH])
+        total_size += child[D_IDX_TOTAL_SIZE]
+
+    hash_str = h.hexdigest()
+    dir_entry[D_IDX_HASH] = hash_str
+    dir_entry[D_IDX_TOTAL_SIZE] = total_size
+    logging.debug('Hash for %s is %s' % (dir_entry[D_IDX_PATH], dir_entry[D_IDX_HASH]))
+    hash_dict.setdefault(hash_str, []).append(dir_entry[D_IDX_PATH]) 
+    cursor.execute('update files set hash = ?, total_size = ? where id = ?',
+                   (hash_str, total_size, dir_entry[D_IDX_ID]))
+
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s', level = logging.INFO)
 
@@ -91,31 +115,13 @@ if __name__ == '__main__':
                          nlink  integer,
                          hash   text,
                          size   integer,
-                         mtime  real)''')
+                         mtime  real,
+                         total_size integer)''')
     conn.commit()
-
-
-    (FTYPE_FIFO, 
-     FTYPE_CHR, 
-     FTYPE_DIR,
-     FTYPE_BLOCK, 
-     FTYPE_REG, 
-     FTYPE_LNK, 
-     FTYPE_SOCK) = (0010000,
-                    0020000,
-                    0040000,
-                    0060000,
-                    0100000,
-                    0120000,
-                    0140000)
 
     file_count = 0
     byte_count = 0
     last_commit_count = 0
-    
-    files_per_commit = 1000
-    count = 0
-    blocksize = 1024 * 1024
 
     is_root = True
 
@@ -126,11 +132,16 @@ if __name__ == '__main__':
     if args.exclude:
         logging.info('Excluding path: %s' % args.exclude)
 
-    for dirpath, dirnames, filenames in os.walk(args.path):        
+    # Walk the directory tree in top-down order (directories visited after files)
+    for dirpath, dirnames, filenames in os.walk(args.path, topdown = True):        
         file_count += 1
+
+        # sort directory names (allowed with topdown visitation)
+        dirnames.sort()
 
         if args.exclude and args.exclude == dirpath:
             logging.info('Skipping %s dirpath' % dirpath)
+            del dirnames[:]
             continue
 
         if is_root:
@@ -140,7 +151,7 @@ if __name__ == '__main__':
         else:
             parent_dir = os.path.dirname(dirpath)
             parent = dir_data[parent_dir]
-            parent_id = parent[0]
+            parent_id = parent[D_IDX_ID]
 
         # Insert Directory
         dstat = os.lstat(dirpath)
@@ -160,11 +171,11 @@ if __name__ == '__main__':
         dir_id = cursor.lastrowid
         file_ents = []
         dir_ents = []
-        cur_dir_ent = [dir_id, parent_id, dirpath, dstat, dir_ents, file_ents, None] # id, parent_id, stat info, dir children, file children, hash
+        cur_dir_ent = [dir_id, parent_id, dirpath, dstat, dir_ents, file_ents, None, 0] # id, parent_id, stat info, dir children, file children, hash, total_size
         dir_data[dirpath] = cur_dir_ent
         # add directory to parent
         if parent:
-            parent[4].append(cur_dir_ent)
+            parent[D_IDX_CHILD_DIRS].append(cur_dir_ent)
 
         if parent_id is None:
             parent_id = -1
@@ -181,11 +192,11 @@ if __name__ == '__main__':
             # Only hash the contents regular files
             if stat.S_ISREG(pstat.st_mode):
                 with open(fullname, 'rb') as f:
-                    data = f.read(blocksize)
+                    data = f.read(HASH_READ_BLOCKSIZE)
                     while len(data) > 0:
                        h.update(data)
                        byte_count += len(data)
-                       data = f.read(blocksize)
+                       data = f.read(HASH_READ_BLOCKSIZE)
 
             hash_str = h.hexdigest()
                        
@@ -204,7 +215,7 @@ if __name__ == '__main__':
  
             file_ents.append((fullname, cursor.lastrowid, dir_id, pstat, hash_str)) 
             file_count += 1
-            if file_count - last_commit_count > files_per_commit:
+            if file_count - last_commit_count > FILES_PER_COMMIT:
                 conn.commit()
                 last_commit_count = file_count
             
